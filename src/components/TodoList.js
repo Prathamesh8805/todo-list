a70232eb6b86b8be67381a3d405c4d65825ba128
prathamesh8805 <bhongaleprathamesh@gmail.com>
2022-03-25 10:14:38 +0530

    Commit 1(25 March)

---
+++ src/components/TodoList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useState } from "react";
+import TodoForm from "./TodoForm";
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    console.log(...todos);
+  };
+
+  return (
+    <div>
+      <h1>
+        What is plan for today?
+        <TodoForm onSubmit={addTodo} />
+      </h1>
+    </div>
+  );
+}
+
+export default TodoList;
