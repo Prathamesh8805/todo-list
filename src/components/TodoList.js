b96bdb349071cc4de224be1ec53e58ce337a153f
prathamesh8805 <bhongaleprathamesh@gmail.com>
2022-03-25 14:03:40 +0530

    Commit 2(25 March)

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import Todo from "./Todo";
 import TodoForm from "./TodoForm";
 
 function TodoList() {
@@ -15,12 +16,40 @@ function TodoList() {
     console.log(...todos);
   };
 
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+    setTodos(removeArr);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id == id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
   return (
     <div>
-      <h1>
-        What is plan for today?
-        <TodoForm onSubmit={addTodo} />
-      </h1>
+      <h1>What is plan for today?</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
     </div>
   );
 }
